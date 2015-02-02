f314be018d700967f96609c8d40a0cfe36dfdd1a
Kamal Pabla and Nathan Sentjens <pair+kpabla+nsentjens@pivotal.io>
2015-02-02 13:55:23 -0500

    support room subject

--- Extensions/XEP-0045/XMPPRoom.h
+++ Extensions/XEP-0045/XMPPRoom.h
@@ -312,4 +312,6 @@ static NSString *const XMPPMUCOwnerNamespace = @"http://jabber.org/protocol/muc#
 - (void)xmppRoom:(XMPPRoom *)sender didEditPrivileges:(XMPPIQ *)iqResult;
 - (void)xmppRoom:(XMPPRoom *)sender didNotEditPrivileges:(XMPPIQ *)iqError;
 
+- (void)xmppRoom:(XMPPRoom *)room roomSubjectChanged:(NSString *)roomSubject;
+
 @end
